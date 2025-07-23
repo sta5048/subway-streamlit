11e0db2095450e317085cdeefb2f69e7449ace38
sta5048 <esder5048@gmail.com>
2025-07-23 13:50:53 +0900

    Add files via upload

--- set_korean_font.py
+++ set_korean_font.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib import font_manager as fm
 import os
-import urllib.request
 
 def setup_korean_font():
-    font_url = "https://github.com/google/fonts/raw/main/ofl/notosanskr/NotoSansKR-Regular.ttf"
-    font_path = "/tmp/NotoSansKR-Regular.ttf"
-    
-    if not os.path.exists(font_path):
-        urllib.request.urlretrieve(font_url, font_path)
-
-    fm.fontManager.addfont(font_path)
-    plt.rcParams["font.family"] = "Noto Sans KR"
+    font_path = os.path.join("fonts", "NotoSansKR-Regular.ttf")
+    if os.path.exists(font_path):
+        fm.fontManager.addfont(font_path)
+        plt.rcParams["font.family"] = "Noto Sans KR"
+    else:
+        print("❗ 폰트 파일이 존재하지 않습니다. fonts/NotoSansKR-Regular.ttf 확인 필요.")
